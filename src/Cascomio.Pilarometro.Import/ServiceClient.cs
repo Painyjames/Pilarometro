12f62b89807a6ab97bff092fa5960415dcb92a47
unknown <painyjames@gmail.com>
2015-09-29 22:15:34 +0100

    Eduardo stuff

--- src/Cascomio.Pilarometro.Import/ServiceClient.cs
+++ src/Cascomio.Pilarometro.Import/ServiceClient.cs
@@ -12,32 +12,52 @@ namespace Cascomio.Pilarometro.Import
 {
 	public class ServiceClient : IServiceClient
 	{
-		private const string Url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=bar|restaurant|point_of_interest&location=41.6488226,-0.8890853&key=AIzaSyDlnS1Di9cRmG8ICqGa7qD1dHfv65zdizY&radius=5000";
+
+		private const double Top = 41.710261;
+		private const double Bottom = 41.607994;
+		private const double Left = -0.975094;
+		private const double Right = -0.781354;
+		private const double Precision = 0.01;
+		private const string Url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=bar|restaurant|point_of_interest&location={0},{1}&key=AIzaSyCeT-vkL-U_0L0LaMJicDwXu_5-G0ZiyfA&radius=500";
 
 		public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterest()
 		{
-			IEnumerable<PointOfInterest> pointsOfInterest = null;
+			var tasks = new List<Task>();
+			var pointsOfInterest = new List<PointOfInterest>();
 			using (var client = new HttpClient(new ManagedHandler()))
 			{
-				await client.GetAsync(Url)
-				.ContinueWith(c =>
+
+				double actualLatitude = Top;
+				while (actualLatitude > Bottom)
 				{
-					var json = c.Result.Content.ReadAsStringAsync();
-					json.Wait();
-					pointsOfInterest = JObject.Parse(json.Result).SelectToken("results")
-									   .ToObject<List<PointOfInterestImport>>()
-									   .Select(s => new PointOfInterest
-									   {
-										   Id = s.Id,
-										   Name = s.Name,
-										   Rating = s.Rating,
-										   Coordinates = new Coordinates
-										   {
-											   Lat = s.Geometry?.Location?.Lat,
-											   Lon = s.Geometry?.Location?.Lng
-										   }
-									   });
-                });
+					double actualLongitude = Left;
+					while (actualLongitude < Right)
+					{
+						tasks.Add(client.GetAsync(string.Format(Url, actualLatitude, actualLongitude))
+						.ContinueWith(c =>
+						{
+							var json = c.Result.Content.ReadAsStringAsync();
+							json.Wait();
+							pointsOfInterest.AddRange(JObject.Parse(json.Result).SelectToken("results")
+											   .ToObject<List<PointOfInterestImport>>()
+											   .Select(s => new PointOfInterest
+											   {
+												   Id = s.Id,
+												   Name = s.Name,
+												   Rating = s.Rating,
+												   Coordinates = new Coordinates
+												   {
+													   Lat = s.Geometry?.Location?.Lat,
+													   Lon = s.Geometry?.Location?.Lng
+												   }
+											   }));
+						}));
+						actualLongitude = actualLongitude + Precision;
+					}
+
+					actualLatitude = actualLatitude - Precision;
+				}
+				Task.WaitAll(tasks.ToArray());
             }
 			return pointsOfInterest;
         }
